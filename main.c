8c38d4911c19b3b7e9d56d51dba9f5606190938c
xiaoyean <xiaoyean@xiaoyeandeMacBook.local>
2017-01-11 01:16:24 +0800

    find references - gtags

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 
 #include "dev_pub.h"
 
+/* gtags test */
+void test1()
+{
+    return;
+}
+
+void test2()
+{
+    test1();
+    return;
+}
+
 int main()
 {
     U32 i;
@@ -9,5 +21,6 @@ int main()
     printf("hello mac!\n\rhello firedragon fruit!\n");
     printf("\rIn 64bits system: U64=%lu,U32=%lu,U16=%lu\n", sizeof(U64), sizeof(U32),sizeof(U16));
 
+    test1();
     return 0;
 }
